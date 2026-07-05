fee0d52fa8d40cd44fe0ff84f8f8b55c867a8087
darshanb12345 <darshan.b@mobioxy.com>
2026-07-05 12:27:11 +0530

    1.two-sum.js

---
+++ arrays/1.two-sum.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            const sum = nums[i] + nums[j];
+            if (sum == target) {
+                return [i, j];
+            }
+        }
+    }
+};
+// @lc code=end
+
