555923e282d01221f4a869669e70ee258bbde6ab
CodeByDarshanDev <bhailalbhaipatel1999@gmail.com>
2026-07-15 23:49:54 +0530

    34.find-first-and-last-position-of-element-in-sorted-array

---
+++ arrays/34.find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=34 lang=javascript
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+    if (nums.length < 1) return [-1, -1];
+
+    return [nums.indexOf(target), nums.lastIndexOf(target)];
+};
+// @lc code=end
+
